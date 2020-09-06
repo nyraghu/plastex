e20d1815eebb3b6a4788c118c4c0c58e438ff22c
N. Raghavendra <raghu@hri.res.in>
2020-09-06 17:42:10 +0530

    Remove colour-modifications branch from git.mk
    * misc/git.mk (GIT_BRANCHES): Remove the colour-modifications branch
      because its work is done.

--- misc/git.mk
+++ misc/git.mk
@@ -9,7 +9,6 @@ GIT_REMOTES = origin upstream
 GIT_REMOTE_ORIGIN_URL = git@github.com:nyraghu/${PROJECT}.git
 GIT_REMOTE_UPSTREAM_URL = https://github.com/plastex/plastex.git
 GIT_BRANCHES = master
-GIT_BRANCHES += colour-modifications
 GIT_CURRENT_BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
 
 ### ==================================================================
